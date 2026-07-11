a1469b27e5d815ac6410cb6007bf6b59aa0a83d2
773172733-jpg <773172733@qq.com>
2026-07-12 00:27:17 +0800

    fix: wall scale 0.35→0.12

--- js/map/DecorationRenderer.js
+++ js/map/DecorationRenderer.js
@@ -192,9 +192,9 @@ class DecorationRenderer {
   getWallConfigs() {
     if (this._wallConfigs) return this._wallConfigs;
     this._wallConfigs = {
-      wall_horizontal: { spriteKey: "wall_horizontal", spritePath: "assets/textures/wall/wall_horizontal_01.png", renderScale: 0.35 },
-      wall_vertical: { spriteKey: "wall_vertical", spritePath: "assets/textures/wall/wall_vertical_01.png", renderScale: 0.35 },
-      wall_corner: { spriteKey: "wall_corner", spritePath: "assets/textures/wall/wall_corner_L_01.png", renderScale: 0.35 }
+      wall_horizontal: { spriteKey: "wall_horizontal", spritePath: "assets/textures/wall/wall_horizontal_01.png", renderScale: 0.12 },
+      wall_vertical: { spriteKey: "wall_vertical", spritePath: "assets/textures/wall/wall_vertical_01.png", renderScale: 0.12 },
+      wall_corner: { spriteKey: "wall_corner", spritePath: "assets/textures/wall/wall_corner_L_01.png", renderScale: 0.12 }
     };
     return this._wallConfigs;
   }
